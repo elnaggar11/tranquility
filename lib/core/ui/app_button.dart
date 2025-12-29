2b533eef906b7ede926415ee2694a832ef29c488
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-12-29 13:37:48 +0200

    delete unuseful folders and setup screen util

--- lib/core/ui/app_button.dart
+++ lib/core/ui/app_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:tharad/core/ui/app_image.dart';
+import 'package:tranquility/core/ui/app_image.dart';
 
 class AppButton extends StatelessWidget {
   const AppButton({
