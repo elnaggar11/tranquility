c83bd02a5aef17efcf433a4aef0e616e4263c5e6
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2026-01-07 16:38:48 +0200

    create chat page with empty chat

--- lib/views/home/pages/chat_page.dart
+++ lib/views/home/pages/chat_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tranquility/core/ui/app_image.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('fffffffffffffff'));
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Theme.of(context).primaryColor,
+        child: AppImage(image: 'flaot_chat.svg'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AppImage(image: 'chat_body.png'),
+            SizedBox(height: 8.h),
+            Text(
+              'You don’t have any Chats yet.',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
