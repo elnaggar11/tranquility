555b2ca038990f607d835bbc7d4c8176b3fddfc1
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2026-01-07 16:11:44 +0200

    create nav bar in home view

---
+++ lib/views/home/view.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:tranquility/core/ui/app_image.dart';
+import 'package:tranquility/views/home/pages/chat_page.dart';
+import 'package:tranquility/views/home/pages/profile_page.dart';
+import 'package:tranquility/views/home/pages/quotes_page.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int currentIndex = 0;
+  final _list = [
+    _Model(
+      icon: 'chat.svg',
+      page: ChatPage(),
+      title: 'Chats',
+      activeIcon: 'chats_active.svg',
+    ),
+    _Model(
+      icon: 'quote.svg',
+      page: QuotesPage(),
+      title: 'Quotes',
+      activeIcon: 'quote_active.svg',
+    ),
+    _Model(
+      icon: 'profile.svg',
+      page: ProfilePage(),
+      title: 'Profile',
+      activeIcon: 'profile_active.svg',
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _list[currentIndex].page,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Colors.white,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        currentIndex: currentIndex,
+        items: List.generate(
+          _list.length,
+          (index) => BottomNavigationBarItem(
+            icon: AppImage(image: _list[index].icon),
+            label: _list[index].title,
+            activeIcon: AppImage(image: _list[index].activeIcon),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Model {
+  final String icon, activeIcon, title;
+
+  final Widget page;
+
+  _Model({
+    required this.icon,
+    required this.page,
+    required this.title,
+    required this.activeIcon,
+  });
+}
